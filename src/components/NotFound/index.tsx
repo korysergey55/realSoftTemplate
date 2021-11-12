b3ad532122812cc08200d4e93c13c9d05bcaf2cc
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-12 17:00:15 +0200

    not-found-props

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -4,20 +4,23 @@ import styles from './styles.module.scss'
 import notFoundImg from '../../sources/images/404.jpg'
 
 interface IProps {
-  img?: any
+  title: string
+  img: string
 }
 
-const NotFoundComponent: React.FC<IProps> = img => {
+const NotFoundComponent: React.FC<IProps> = ({
+  title = 'We Could Not Find Page You’re Looking',
+  img = notFoundImg,
+}) => {
   const history = useHistory()
+  console.log(notFoundImg)
   return (
     <div className={styles.container}>
       <div className={styles.wripper}>
-        <img className={styles.img} src={notFoundImg} alt="" />
+        <img className={styles.img} src={img} alt="" />
         <div className={styles.rigthSide}>
           <h2 className={styles.title}>404</h2>
-          <h3 className={styles.subtitle}>
-            We Could Not Find Page You’re Looking
-          </h3>
+          <h3 className={styles.subtitle}>{title}</h3>
           <p className={styles.text}>
             The link you’re trying to access is probably broken, or the page has
             been removed.

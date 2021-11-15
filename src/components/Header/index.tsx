669c1088dda40e83fd8b928a3b587db0d6780594
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-15 16:20:53 +0200

    header-refactoring

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
-import Time from 'utils/Time/Time'
 import styles from './styles.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShippingFast } from '@fortawesome/free-solid-svg-icons'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
 
 interface Iprops {
   logo?: string
   title?: string
 }
 
-const Header: React.FC<Iprops> = ({ logo = '', title = 'Shipping-Fast' }) => {
+const Header: React.FC<Iprops> = ({ logo = '', title = 'Header' }) => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <FontAwesomeIcon
-          icon={faShippingFast}
-          color="blue"
-          size="3x"
           className={styles.icon}
+          icon={faReact}
+          color="red"
+          size="3x"
         />
-        {logo && <img src="logo" alt="logo" />}
+        {logo && <img src={logo} alt="logo" />}
         <h2 className={styles.title}>{title}</h2>
-        <Time />
       </div>
     </header>
   )
--- src/containers/Public/HomePage/index.tsx
+++ src/containers/Public/HomePage/index.tsx
@@ -1,10 +1,14 @@
+import Header from 'components/Header'
 import React from 'react'
 import styles from './styles.module.scss'
 
 const HomePage = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>HomePage</h2>
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <h2 className={styles.title}>HomePage</h2>
+      </div>
     </div>
   )
 }

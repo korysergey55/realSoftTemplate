d79ee9d761567c9ec96ebdf95bbabe07e7151209
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-15 17:27:42 +0200

    svg-icon-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { paths } from 'utils/paths'
 import { useHistory } from 'react-router'
+import { paths } from 'utils/paths'
 import styles from './styles.module.scss'
+import sprite from 'sources/icons/sprite.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShippingFast } from '@fortawesome/free-solid-svg-icons'
 import { faReact } from '@fortawesome/free-brands-svg-icons'
@@ -17,15 +18,18 @@ const Header: React.FC<Iprops> = ({ logo = '', title = 'Header' }) => {
     <header className={styles.header}>
       <div className={styles.container}>
         <a className={styles.link} onClick={() => history.push(paths.home)}>
-          <FontAwesomeIcon
-            className={styles.icon}
-            icon={faReact}
-            color="red"
-            size="3x"
-          />
+          <svg className={styles.iconHome}>
+            <use href={sprite + '#icon-home'} />
+          </svg>
         </a>
         {logo && <img className={styles.logo} src={logo} alt="logo" />}
         <h2 className={styles.title}>{title}</h2>
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={faReact}
+          color="red"
+          size="3x"
+        />
       </div>
     </header>
   )
